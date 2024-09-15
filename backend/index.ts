3f8c721430a1cce5f48f8a9d47f55cda48919526
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-09-15 19:56:48 +0530

    db connceted

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB')
+})
+.catch((err) => {
+    console.log(err)
+})
+
+console.log('here')
 const app = express()
 const PORT: number = 3000
 
