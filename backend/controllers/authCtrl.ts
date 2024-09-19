22ac4ea173f6178efb4ec4d08e3ede7df36c63f2
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-09-19 20:27:46 +0530

    hashed passwrod

--- backend/controllers/authCtrl.ts
+++ backend/controllers/authCtrl.ts
@@ -1,9 +1,17 @@
 import { Request, Response} from 'express'
 import User from '../models/userModel.js'
+import bcryptjs from 'bcryptjs'
 
 export const signup = async (req: Request, res: Response) => {
-    const {userName, email, password} = req.body
-    const newUser = new User({userName, email, password})
-     await newUser.save()
-     res.status(201).json({message: "User creted successfully!"})
+    const {username, email, password} = req.body
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({username, email, password: hashedPassword})
+    try{
+        await newUser.save()
+        res.status(201).json({message: "User creted successfully!"})
+    }catch(err: any) {
+
+        res.status(500).json({message: err.message})
+    }
+   
 }
