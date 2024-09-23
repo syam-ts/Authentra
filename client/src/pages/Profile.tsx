165c4e0eef6cbcbcadb82b888758c9984a31e9ca
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-09-23 21:06:07 +0530

    profile page finished

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {useSelector} from 'react-redux'
 
 function Profile() {
+   const { user } = useSelector((state: any ) => state.user)
+
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'> Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={user.currentUser.profilePicture} alt='profile' 
+         className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
+        />
+        <input defaultValue={user.currentUser.username} type='text' id='username'  
+          placeholder='username' className='bg-slate-100 rounded-lg p-3'
+        />
+        <input defaultValue={user.currentUser.email} type='email' id='email'  
+          placeholder='email' className='bg-slate-100 rounded-lg p-3'
+        />
+        <input type='password' id='password'  
+          placeholder='password' className='bg-slate-100 rounded-lg p-3'
+        />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          Update
+        </button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
 
